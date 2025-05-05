d94a82e618fe3e9f0c31688f39762fdecc2b5ad7
BiancaAlFe <biancaalfe5@gmail.com>
2025-05-05 18:52:10 -0300

    Added Prototype Player Movement

---
+++ Assets/Scenes/Scripts/PlayerManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public float Speed;
+    public Rigidbody2D Rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float XAxis = Input.GetAxis("Horizontal");
+        float YAxis = Input.GetAxis("Vertical");
+        Vector3 Axis = new Vector3(XAxis, YAxis, 0f);
+        if (Axis.magnitude > 1f)
+        {
+            Axis.Normalize();
+        }
+
+        Rigidbody.MovePosition(transform.position + Axis * Time.deltaTime);
+    }
+}
