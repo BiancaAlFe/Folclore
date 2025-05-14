eb5f54c5047007619aa8db77f813f5c498799f3a
BiancaAlFe <biancaalfe5@gmail.com>
2025-05-14 14:04:31 -0300

    Added placeholder map and character

--- Assets/Scripts/SpellScript.cs
+++ Assets/Scripts/SpellScript.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpellScript : MonoBehaviour
+public abstract class SpellScript : MonoBehaviour
 {
     public GameObject Player;
     public Vector3 CastPosition;
     public float Speed, Damage;
     public List<string> TargetTag;
-    public virtual void Cast() {}
+    public abstract void Cast();
 }
